6ee6ce0c245a8b352df96f8a1a1d1a2fd22fddcf
diegoalonzomora <u20231c069@upc.edu.pe>
2026-08-23 14:12:10 -0500

    chore: add transform on schema

--- model/course.ts
+++ model/course.ts
@@ -3,8 +3,8 @@ import * as z from 'zod';
 export const CourseSchema = z.object({
     id : z.uuid().optional(),
     name : z.string(),
-    initTime: z.iso.date(),
-    finalTime: z.iso.date()
+    initTime: z.iso.time({precision: 0}).transform((str)=> new Date(`1970-01-01T${str}Z`)),
+    finalTime: z.iso.time({precision: 0}).transform((str)=> new Date(`1970-01-01T${str}Z`))
 })
 
 export type Course = z.infer<typeof CourseSchema>;
