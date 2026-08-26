860bb7b2d7de11efb7214b342d4cff3c5e27b280
billyelcalvo <dam0806blas@gmail.com>
2026-08-26 11:38:10 -0500

    feat: notification delete from the project, it's not useful at all

--- model/notification.ts
+++
@@ -1,8 +0,0 @@
-import * as z from 'zod';
-
-export const NotificationSchema = z.object({
-        id : z.uuid().optional(),
-        idCourse: z.uuid()
-});
-
-export type Notification = z.infer<typeof NotificationSchema>;
